fa80b9eb3d106d6472de76828d3feb4478b55ee8
MarcinNieckosinski <marcin.nieckosinski@gmail.com>
2024-12-11 14:19:01 +0100

    minor changes day_2

--- day_2/solution.py
+++ day_2/solution.py
@@ -1,18 +1,13 @@
 def read_input():
     # input should have one blank line at the end
-    reports = []
     with open('input.txt', 'r') as file:
         lines = file.readlines()
-    for line in lines:
-        reports.append(line[:-1])
+    reports = [(line[:-1]) for line in lines]
     return reports
 
 
 def reports_to_list(reports):
-    new_reports = []
-    for report in reports:
-        new_reports.append(list(map(int, report.split(' '))))
-    return new_reports
+    return [list(map(int, report.split(' '))) for report in reports]
 
 
 def check_asc_or_desc(reports):
@@ -20,12 +15,14 @@ def check_asc_or_desc(reports):
     for report in reports:
         if check_asc_or_desc_single(report):
             asc_or_desc_reports.append(report)
+    a = [report for report in reports if check_is_safe_report(report)]
     return asc_or_desc_reports
 
 
 def check_asc_or_desc_single(report):
     return all(report[i] < report[i + 1] for i in range(len(report) - 1)) or all(
         report[i] > report[i + 1] for i in range(len(report) - 1))
+
 
 def check_is_safe_report(report):
     return all(-3 <= (report[i + 1] - report[i]) <= 3 for i in range(len(report) - 1))
@@ -38,6 +35,7 @@ def get_safe_reports(reports):
             safe_reports.append(report)
     return safe_reports
 
+
 def get_safe_reports_with_removal(reports):
     safe_reports = []
     for report in reports:
@@ -49,11 +47,9 @@ def get_safe_reports_with_removal(reports):
                     break
     return safe_reports
 
+
 r = read_input()
 r = reports_to_list(r)
 asc_desc_r = check_asc_or_desc(r)
-safe_r = get_safe_reports(asc_desc_r)
-first_answer = len(safe_r)
-
-safe_r_after_removal = get_safe_reports_with_removal(r)
-second_answer = len(safe_r_after_removal)
+first_answer = len(get_safe_reports(asc_desc_r))
+second_answer = len(get_safe_reports_with_removal(r))
